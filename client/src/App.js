abe561d6106a8321a3719e4c7d8df42da10b679a
ldv432 <ldv432@gmail.com>
2025-03-22 11:43:19 -0600

    readme created, basic landing page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
+import React from "react";
+import WelcomePage from "./pages/WelcomePage"; 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <WelcomePage /> 
       </header>
     </div>
   );
